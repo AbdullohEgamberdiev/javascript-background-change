3f7982a8ba5a39739f01ab2544e956c070564fc4
abdullohegamberdiyev007@gmail.com <abdullohegamberdiyev007@gmail.com>
2022-12-01 10:04:04 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const button = document.getElementById('btn');
+const body = document.querySelector('body');
+const color = ['red', 'green', 'blue', 'yellow', 'pink', 'grey', 'lime', 'purple', 'violet', 'aqua'];
+
+body.style.backgroundColor = "black";
+button.addEventListener('click', () => {
+  const colorIndex = parseInt(Math.random() * color.length);
+  body.style.backgroundColor = color[colorIndex];
+  body.style.transition = '1s linear';
+});
+
